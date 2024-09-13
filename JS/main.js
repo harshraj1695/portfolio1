21d4ef507c6434ad60ef60be9e3b75b60c1a47d4
harshraj1695 <singhharshraj215@gmail.com>
2024-09-13 23:31:28 +0530

    measa

---
+++ JS/main.js
@@ -0,0 +1,56 @@
+let menuIcon =document.querySelector('#menu-icon');
+let navbar=document.querySelector('.navbar');
+
+menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+}
+
+
+let sections=document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll=()=>{
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop-150;
+        let height =sec.offsetHeight;
+        let id =sec.getAttribute('id');
+        if(top>= offset && top <offset + height){
+            navLinks.forEach.apply(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky',window.scrollY>100);
+
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+
+};
+
+
+ScrollReveal({
+    distance:'80px',
+    duration:2000,
+    delay: 200,
+
+});
+ScrollReveal().reveal('.home-content, heading',{origin: 'top'});
+ScrollReveal().reveal('.home-img, .services-containe, .portfolio-box, .contact form',{origin: 'buttom'});
+ScrollReveal().reveal('.home-contact h1, .about-img',{origin: 'left'});
+ScrollReveal().reveal('.home-contact p , .about-content',{origin: 'right'});
+
+
+//typed js
+const typed = new Typed('.mutiple-text',{
+    strings:['Fullstack Developer', 'DevOps Enthusiast '],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay:1000,
+    loop: true,
+}) ;
