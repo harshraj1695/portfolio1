38530db247dbebc75b0446806ebce6edd73e5c40
harshraj1695 <singhharshraj215@gmail.com>
2024-09-14 18:22:34 +0530

    addded all the projects in the project section

--- JS/main.js
+++ JS/main.js
@@ -41,7 +41,7 @@ ScrollReveal({
 
 });
 ScrollReveal().reveal('.home-content, heading',{origin: 'top'});
-ScrollReveal().reveal('.home-img, .services-containe, .portfolio-box, .contact form',{origin: 'buttom'});
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form',{origin: 'buttom'});
 ScrollReveal().reveal('.home-contact h1, .about-img',{origin: 'left'});
 ScrollReveal().reveal('.home-contact p , .about-content',{origin: 'right'});
 
